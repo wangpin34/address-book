168b3cc9536edf7db052b2482ada12c69cf14bcd
wangpin34 <270280522@qq.com>
2020-05-27 10:50:37 +0800

    Updates

---
+++ precache-manifest.29345dc56195c4a6eb875d43a4ac0737.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85d520a7c7a5c42c3bd5e5712ffa8cf2",
+    "url": "http://blog.wangpin.xyz/address-book/index.html"
+  },
+  {
+    "revision": "b8a3114c81694df201fb",
+    "url": "http://blog.wangpin.xyz/address-book/static/css/2.63b65d4e.chunk.css"
+  },
+  {
+    "revision": "e7b32010623efc9230da",
+    "url": "http://blog.wangpin.xyz/address-book/static/css/main.24dd0505.chunk.css"
+  },
+  {
+    "revision": "b8a3114c81694df201fb",
+    "url": "http://blog.wangpin.xyz/address-book/static/js/2.c6680f32.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "http://blog.wangpin.xyz/address-book/static/js/2.c6680f32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7b32010623efc9230da",
+    "url": "http://blog.wangpin.xyz/address-book/static/js/main.0a7516dd.chunk.js"
+  },
+  {
+    "revision": "dac51b13fa4eb61d75b5",
+    "url": "http://blog.wangpin.xyz/address-book/static/js/runtime-main.c7b03a36.js"
+  }
+]);
